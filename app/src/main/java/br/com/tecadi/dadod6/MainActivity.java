2a3020c94c4e27f27a7447c0b44ea14f37d25132
Kalebe Jucelio da Silva <73969355+Kalebe-Silva@users.noreply.github.com>
2021-10-30 09:42:25 -0300

    first commit

---
+++ app/src/main/java/br/com/tecadi/dadod6/MainActivity.java
@@ -0,0 +1,63 @@
+package br.com.tecadi.dadod6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView dado;
+    private ImageView dado1;
+
+    private int[] numerosDoDado = {
+            R.drawable.diceum,
+            R.drawable.dicedois,
+            R.drawable.dicetres,
+            R.drawable.dicequatro,
+            R.drawable.dicecinco,
+            R.drawable.diceseis
+    };
+
+    private int[] numerosDoDado1 = {
+            R.drawable.diceum,
+            R.drawable.dicedois,
+            R.drawable.dicetres,
+            R.drawable.dicequatro,
+            R.drawable.dicecinco,
+            R.drawable.diceseis
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dado = findViewById(R.id.dado);
+
+        dado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int numerosRandom = new Random().nextInt(numerosDoDado.length);
+                dado.setImageResource(numerosDoDado[numerosRandom]);
+
+            }
+        });
+
+        dado1 = findViewById(R.id.dado1);
+
+        dado1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int numerosRandom = new Random().nextInt(numerosDoDado1.length);
+                dado1.setImageResource(numerosDoDado1[numerosRandom]);
+
+            }
+        });
+    }
+}
